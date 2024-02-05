dd13e9f53058faed143fdbcf6fc1f422f32a61ed
jelkesudo <ficajelic@gmail.com>
2024-02-05 02:30:18 +0100

    Update script.js
    update gif restarting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 window.onload = function(){
+    const d = new Date();
+    let time = d.getTime();
+    let srcImage = `assets/img/logo.gif?${time}`;
+    console.log(srcImage);
+    $("#pictureLoad").attr("src", srcImage);
     setTimeout(function(){
         $("#loader").addClass("removeIt");
     }, 2000);
