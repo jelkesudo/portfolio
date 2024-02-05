47bfb0fdecea9cec4b10014310a7785ad2215e86
jelkesudo <ficajelic@gmail.com>
2024-02-05 20:22:49 +0100

    printing full timeline, data for timeline

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,66 @@ window.onload = function(){
     const d = new Date();
     let time = d.getTime();
     let srcImage = `assets/img/logo.gif?${time}`;
+
     $(document.body).css("overflow","hidden");
     $("#pictureLoad").attr("src", srcImage);
+    
     setTimeout(function(){
         $("#loader").addClass("fadeOut");
         setTimeout(function() {
             $("#loader").css("display", "none");
         }, 500);
-    $(document.body).css("overflow","visible");
-
+        $(document.body).css("overflow","visible");
     }, 2000);
+    ajaxCallBack("assets/data/timeline.json", function(result){
+        printTimeline(result);
+    });
 }
+
+function printTimeline(data){
+    let html = "";
+    for(let d of data){
+        let temp = printSimpleTimelineUnit(d);
+        if(d.hasOwnProperty("image")){
+            temp = printImageTimelineUnit(d)
+        }
+        html += temp;
+    }
+    $("#printTimeline").html(html);
+}
+
+function printSimpleTimelineUnit(d){
+    return `<li class="event">
+    <h3>${d.title}</h3>
+    <p>${d.description}</p>
+  </li>`;
+}
+
+function printImageTimelineUnit(d){
+    return `<li class="event">
+    <div class="project">
+      <div class="imageProject">
+        <img src="assets/img/${d.image}" alt="portfolio">
+      </div>
+      <div class="descriptionProject">
+        <div class="title"><h3>${d.title}</h3></div>
+        <div class="description"><p>${d.description}</p></div>
+        <a href="${d.href}" target="_blank"><div class="linkButton fj-w50 fj-mx-auto">Go to page</div></a>
+      </div>
+    </div>
+  </li>`;
+}
+
+function ajaxCallBack(u, s){
+    $.ajax({
+        url: u,
+        method: "GET",
+        data: "",
+        dataType: "json",
+        success: s,
+        error: function(xhr){
+            console.error(xhr);
+        }
+    });
+}
+
