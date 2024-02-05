b27b32c9afe73f6d3ac1d1055c8e609875405572
jelkesudo <ficajelic@gmail.com>
2024-02-05 18:38:22 +0100

    adding pictures, testing timeline

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,11 @@ window.onload = function(){
     const d = new Date();
     let time = d.getTime();
     let srcImage = `assets/img/logo.gif?${time}`;
-    console.log(srcImage);
+    $(document.body).css("overflow","hidden");
     $("#pictureLoad").attr("src", srcImage);
     setTimeout(function(){
-        $("#loader").addClass("removeIt");
+        $("#loader").addClass("fadeOut");
+    $(document.body).css("overflow","visible");
+
     }, 2000);
 }
